2064a85b69e57fdbe1f2f69291db4bb5198ae8a9
lizkund <lizkund@gmail.com>
2016-01-07 14:33:21 -0800

    added index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>  
+    <head>
+        <meta http-equiv="Conent-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <?php
+        // code
+        echo 'hello world';
+        ?>
+        
+    </body>
+</html>
+
+
+
